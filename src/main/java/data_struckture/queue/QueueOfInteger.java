1a31272a5a683f3a4078e6c98235fede8f161bfa
YaroslavGnatyuk <yaroslavforjob@gmail.com>
2017-03-25 05:54:35 +0200

    I begin to realize queue of integer

--- src/main/java/data_struckture/queue/QueueOfInteger.java
+++ src/main/java/data_struckture/queue/QueueOfInteger.java
@@ -1,6 +1,41 @@
 package data_struckture.queue;
 
 public class QueueOfInteger {
-    Integer[] array;
+    private Integer[] array;
+    private int maxSize;
+    private int nElem;
+    private int rear;
+    private int front;
 
+    public QueueOfInteger(int maxSize) {
+        this.maxSize = maxSize;
+        array = new Integer[this.maxSize];
+        front = 0;
+        rear = -1;
+        nElem = 0;
+    }
+
+    public void insert(int value) {
+        if (rear < maxSize && rear < front) {
+            array[++rear] = value;
+            ++nElem;
+        } else if (rear >= maxSize && front > 0) {
+            rear = -1;
+            array[++rear] = value;
+            ++nElem;
+        } else {
+            System.out.println("Queue is full!!!");
+        }
+    }
+
+    public int remove() {
+        return 0;
+    }
+
+    public int peekFront() {
+        if (nElem != 0)
+            return array[front];
+        else
+            return 0;
+    }
 }
--- src/test/java/TestMergeSort.java
+++ src/test/java/TestMergeSort.java
@@ -1,18 +1,8 @@
-import algorithm.merge_sort.MergeSort;
 import org.junit.Test;
-import quick_sort.QuickSort;
-import utils.FillArrayUtil;
 
-/**
- * Created by yroslav on 11/2/16.
- */
 public class TestMergeSort {
     @Test
-    public void merge(){
-        int[] array = FillArrayUtil.RandomArray(100_000);
-        QuickSort quickSort = new QuickSort(array);
-        System.out.println(quickSort.toString());
-        quickSort.sort();
-        System.out.println(quickSort.toString());
+    public void merge() {
+
     }
 }
